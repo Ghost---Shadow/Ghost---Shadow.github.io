a5515cb262355f0892fcfe0d787c018fa533a578
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2021-03-14 19:15:06 +0530

    Experimental rect

--- src/sketch.js
+++ src/sketch.js
@@ -5,6 +5,9 @@ const sketch = (p) => {
 
   p.setup = () => {
     p.createCanvas(canvasWidth, canvasHeight);
+    // p.textSize(16);
+    p.textSize(32);
+    p.textAlign(p.CENTER, p.CENTER);
   };
 
   p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
@@ -17,7 +20,18 @@ const sketch = (p) => {
     p.background(255);
     p.fill(30);
     p.noStroke();
-    p.rect(30, 20, 55, 55, 20);
+    const rectX = 250;
+    const rectY = 100;
+    const rectW = 150;
+    const rectH = 50;
+    p.push();
+    p.translate(rectX + rectW / 2, rectY + rectH / 2);
+    p.rotate(Math.sin(p.frameCount / 10) * (Math.PI / 8));
+    p.rect(-rectW / 2, -rectH / 2, rectW, rectH, 20);
+    p.fill(200);
+    p.text('O wO', 0, 0);
+    // p.text('Download Resume', 0, 0);
+    p.pop();
   };
 };
 /* eslint-enable no-param-reassign */
